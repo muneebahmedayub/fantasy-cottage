147ef7204433f3e516cb58f8032264d04b44481a
muneebahmedayub <muneebahmedayub01@gmail.com>
2023-01-27 19:17:10 +0500

    fix contact form issue

--- js/custom.js
+++ js/custom.js
@@ -73,7 +73,7 @@
       var subject = $("#cf-subject").val();
       var message = $("#cf-message").val();
       var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
-
+      console.log(dataString)
       function isValidEmail(emailAddress) {
           var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
           return pattern.test(emailAddress);
@@ -81,11 +81,23 @@
       if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
           $.ajax({
               type: "POST",
-              url: "email.php",
-              data: dataString,
+              url: "https://nodemailer-production.up.railway.app/api/v1/nodemailer",
+              data: {
+                name,
+                email,
+                subject,
+                description: message,
+                to: "mubashirjavaid@gmail.com"
+              },
+              dataType: 'json',
               success: function () {
                   $('.text-success').fadeIn(1000);
                   $('.text-danger').fadeOut(500);
+              },
+              error: function (err) {
+                console.log({err})
+                  $('.text-danger').fadeIn(1000);
+                  $('.text-success').fadeOut(500);
               }
           });
       }
